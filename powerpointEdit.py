a56fcb9d56e32b88726642eb08f84212a7939e89
taeholee <dlxogh5185@gmail.com>
2022-09-15 09:57:43 -0700

    Second Commit

--- powerpointEdit.py
+++ powerpointEdit.py
@@ -2,8 +2,8 @@ from pptx import Presentation
 from pptx.util import Inches
 
 
-ppt = Presentation("resource/report.potx")
-slide = ppt.slides[0]
+ppt = Presentation("resource/report.pptx")
+slide = ppt.slides[1]
 
 for shape in slide.shapes:
     print(shape.shape_type)
--- scrapper.py
+++ scrapper.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+
 def highlight(element, effect_time, color, border):
     driver = element._parent
 
@@ -25,13 +26,10 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 
 driver = webdriver.Chrome(
-    chrome_options= chrome_options,
+    chrome_options=chrome_options,
     executable_path="/Users/taeholee/Documents/Selnium/Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH01/01_02/chromedriver")
 driver.get("https://talkbot.us/#/login")
 driver.implicitly_wait(10)
-
-
-
 
 uname = driver.find_element(By.ID, "username")
 pw = driver.find_element(By.ID, "password")
@@ -43,7 +41,8 @@ pw.clear()
 pw.send_keys("SaltluxUSVN1234!@#$")
 login.click()
 
-folder = driver.find_element(By.XPATH, '//div[@class="treeList"]//label[@title="Arelli"]/../../../*[@class = "fa font-tree-item fa-folder"]')
+folder = driver.find_element(By.XPATH,
+                             '//div[@class="treeList"]//label[@title="Arelli"]/../../../*[@class = "fa font-tree-item fa-folder"]')
 folder.click()
 
 Project = driver.find_element(By.XPATH, '//*[@class = "treeList"]//span[text()="Astro"]')
@@ -58,12 +57,11 @@ for i in range(3):
 
 driver.find_element(By.XPATH, '//*[@id="nav-left"]/ul/li[last()]').click()
 
+
 # pick a starting date and ending date
 
 
-
 def select_date(start_or_end, date):
-
     month_to_name = {
         "01": "Jan", "02": "Feb", "03": "Mar",
         "04": "Apr", "05": "May", "06": "Jun",
@@ -71,7 +69,7 @@ def select_date(start_or_end, date):
         "10": "Oct", "11": "Nov", "12": "Dec"
     }
 
-    #click date icon
+    # click date icon
     driver.find_elements(By.XPATH, "//div[@class= 'bi-filter-cont']//*[@class='icon-date']")[start_or_end].click()
 
     # Choose a year
@@ -87,11 +85,10 @@ def select_date(start_or_end, date):
 
 
 # starting date
-select_date(0,["2022","08","01"])
+select_date(0, ["2022", "08", "01"])
 
 # ending date
-select_date(1,["2022","08","31"])
-
+select_date(1, ["2022", "08", "31"])
 
 driver.find_element(By.XPATH, "//div[@class= 'bi-filter-cont']//button").click()
 
@@ -101,20 +98,18 @@ screenshot = driver.save_screenshot("screenshot.png")
 
 driver.find_element(By.XPATH, '//*[@class= "chart-content chart-content-sp rb"]/*').screenshot("temp.png")
 
-S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
-driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment
+S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
+driver.set_window_size(S('Width'), S('Height'))  # May need manual adjustment
 
-
-data = driver.find_element(By.XPATH, '//*[@class = "tab-content"]').find_elements(By.XPATH, './div[@class = "tab-content-hor"]')
+data = driver.find_element(By.XPATH, '//*[@class = "tab-content"]').find_elements(By.XPATH,
+                                                                                  './div[@class = "tab-content-hor"]')
 
 i = 1
 for element in data:
     element.screenshot(f'web-element-{i}.png')
-    i+=1
+    i += 1
 
-driver.find_element( By.TAG_NAME, 'body').screenshot('web_screenshot.png')
-
-
+driver.find_element(By.TAG_NAME, 'body').screenshot('web_screenshot.png')
 
 # highlight(Astro, 3, "red", 2)
 # Astro.click()
