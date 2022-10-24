0cd6c0e6694e392964f88db1fef937e07c8a61fd
taeholee <dlxogh5185@gmail.com>
2022-10-24 15:54:20 -0700

    Clunky, but functional

--- powerpointEdit.py
+++ powerpointEdit.py
@@ -1,9 +1,126 @@
+import pptx.text.text
 from pptx import Presentation
 from pptx.util import Inches
+from pptx.util import Pt
+from pptx.dml.color import RGBColor
+# from scrapper import *
+import pandas as pd
+
+year = 2022
+month = 9
+# bot_name = "Astro"
+bot_name = "Nate & Maven"
+month_to_name = {
+    1: "Jan", 2: "Feb", 3: "Mar",
+    4: "Apr", 5: "May", 6: "Jun",
+    7: "Jul", 8: "Aug", 9: "Sep",
+    10: "Oct", 11: "Nov", 12: "Dec"
+}
+
+# page 3
+user_and_messages_img_path = "web-element-2.png"
+messages_img_path = "web-element-3.png"
+
+# page 4
+conversations_img_path = "web-element-4.png"
+irqa_img_path = "web-element-5.png"
+
+
+
+
+def update_date(text: pptx.text.text.TextFrame, month: str, year):
+    text.word_wrap = False
+    p = text.paragraphs[0]
+    p.clear()
+
+    run = text.paragraphs[0].add_run()
+    run.text = bot_name
+    font = run.font
+    font.name = 'Calibri'
+    font.size = Pt(16)
+    font.color.rgb = RGBColor(8, 172, 84)
+
+    run = text.paragraphs[0].add_run()
+    run.text = f" {month} {year}"
+    font = run.font
+    font.name = 'Calibri'
+    font.size = Pt(16)
 
 
 ppt = Presentation("resource/report.pptx")
-slide = ppt.slides[2]
 
+
+# 2nd page
+# slide = ppt.slides[1]
+# filename = get_excel_file_name()
+# df = pd.read_excel(filename, sheet_name="Users and mesages")
+# print(df)
+
+# 3rd page
+slide = ppt.slides[2]
+texts = []
 for shape in slide.shapes:
+    if shape.shape_type == 17:
+        texts.append(shape)
+    elif shape.shape_type == 13:
+        slide.shapes.element.remove(shape.element)
+
+# Freeform 5: page number
+# placeholder 14: Talkbot Report
+# Text_Box 17: Nate & Maven Feb 2022
+# Picture 13: picture
+
+# update date for the 3rd page
+date = texts[0].text_frame
+update_date(date, month_to_name[month], year)
+
+# update graph for the 3rd page
+users_and_messages = slide.shapes.add_picture(user_and_messages_img_path, Inches(0.8), Inches(1.2), width=Inches(10.1))
+messages = slide.shapes.add_picture(messages_img_path, Inches(0.8), Inches(1.2) + users_and_messages.height,
+                                    width=Inches(7))
+
+
+# 4th page
+slide = ppt.slides[3]
+
+
+texts = []
+for shape in slide.shapes:
+    if shape.shape_type == 17:
+        texts.append(shape)
+    elif shape.shape_type == 13:
+        slide.shapes.element.remove(shape.element)
     print(shape.shape_type)
+
+conversations = slide.shapes.add_picture(conversations_img_path, Inches(0.8), Inches(1.2), width=Inches(7))
+irqa = slide.shapes.add_picture(irqa_img_path, Inches(0.8), Inches(1.2) + conversations.height, width=Inches(7))
+
+text = texts[0].text_frame
+update_date(text, month_to_name[month], year)
+
+
+# 5th page
+slide = ppt.slides[4]
+
+print("\n")
+texts = []
+for shape in slide.shapes:
+    if shape.shape_type == 17:
+        texts.append(shape)
+    elif shape.shape_type == 13:
+        slide.shapes.element.remove(shape.element)
+    print(shape.shape_type)
+
+# text_box 0 : page number
+# text_box 1: Nate & Maven Feb 2022
+
+
+
+text = texts[1].text_frame
+
+emails = slide.shapes.add_picture("email.png", Inches(0.8), Inches(1.2), width=Inches(10))
+update_date(text, month_to_name[month], year)
+
+
+
+ppt.save('New_report.pptx')
--- scrapper.py
+++ scrapper.py
@@ -6,8 +6,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import time
-
+import os
 
 def highlight(element, effect_time, color, border):
     driver = element._parent
@@ -25,12 +26,16 @@ def highlight(element, effect_time, color, border):
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
+
+# Initialization
 driver = webdriver.Chrome(
     chrome_options=chrome_options,
     executable_path="resource/chromedriver")
 driver.get("https://talkbot.us/#/login")
 driver.implicitly_wait(10)
 
+
+# Login
 uname = driver.find_element(By.ID, "username")
 pw = driver.find_element(By.ID, "password")
 login = driver.find_element(By.XPATH, "//jhi-talkbot-login//form/div/button")
@@ -41,12 +46,19 @@ pw.clear()
 pw.send_keys("SaltluxUSVN1234!@#$")
 login.click()
 
-folder = driver.find_element(By.XPATH,
-                             '//div[@class="treeList"]//label[@title="Arelli"]/../../../*[@class = "fa font-tree-item fa-folder"]')
+
+
+# Finding Arelli
+# folder = driver.find_element(By.XPATH, '//div[@class="treeList"]//label[@title="Arelli"]/../../../*[@class = "fa font-tree-item fa-folder"]')
+# folder.click()
+# Project = driver.find_element(By.XPATH, '//*[@class = "treeList"]//span[text()="Astro"]')
+# Project.click()
+
+# Finding NavienMate
+folder = driver.find_element(By.XPATH, '//div[@class="treeList"]//label[@title="NavienMate"]')
 folder.click()
 
-Project = driver.find_element(By.XPATH, '//*[@class = "treeList"]//span[text()="Astro"]')
-Project.click()
+driver.implicitly_wait(10)
 
 for i in range(3):
     try:
@@ -61,7 +73,7 @@ driver.find_element(By.XPATH, '//*[@id="nav-left"]/ul/li[last()]').click()
 # pick a starting date and ending date
 
 
-def select_date(start_or_end, date):
+def select_date(start_or_end, tab, date):
     month_to_name = {
         "01": "Jan", "02": "Feb", "03": "Mar",
         "04": "Apr", "05": "May", "06": "Jun",
@@ -69,8 +81,13 @@ def select_date(start_or_end, date):
         "10": "Oct", "11": "Nov", "12": "Dec"
     }
 
+    tab_to_index = {
+        "Overall": 1,
+        "Email": 7
+    }
+
     # click date icon
-    driver.find_elements(By.XPATH, "//div[@class= 'bi-filter-cont']//*[@class='icon-date']")[start_or_end].click()
+    driver.find_elements(By.XPATH, f'//*[@class = "bi-board-tab"]/jhi-tab[position() = {tab_to_index[tab]}]//div[@class = "bi-filter-cont"]//*[@class="icon-date"]')[start_or_end].click()
 
     # Choose a year
     driver.find_elements(By.XPATH, '//span[@class ="owl-dt-control-button-content"]')[1].click()
@@ -85,34 +102,82 @@ def select_date(start_or_end, date):
 
 
 # starting date
-select_date(0, ["2022", "08", "01"])
+select_date(0, "Overall",["2022", "09", "01"])
 
 # ending date
-select_date(1, ["2022", "08", "31"])
+select_date(1, "Overall",["2022", "09", "30"])
 
 driver.find_element(By.XPATH, "//div[@class= 'bi-filter-cont']//button").click()
 
-driver.maximize_window()
+# screenshot = driver.save_screenshot("screenshot.png")
 
-screenshot = driver.save_screenshot("screenshot.png")
+# driver.find_element(By.XPATH, '//*[@class= "chart-content chart-content-sp rb"]/*').screenshot("temp.png")
 
-driver.find_element(By.XPATH, '//*[@class= "chart-content chart-content-sp rb"]/*').screenshot("temp.png")
+# download the data
 
+driver.find_element(By.XPATH, '//*[@class = "bi-download-btn"]/button').click()
+
+def download_wait(path_to_downloads):
+    seconds = 0
+    dl_wait = True
+    while dl_wait and seconds < 20:
+        time.sleep(1)
+        dl_wait = False
+        for fname in os.listdir(path_to_downloads):
+            if fname.endswith('.crdownload'):
+                dl_wait = True
+        seconds += 1
+    return seconds
+
+download_wait("./")
+
+def get_xls_file_name():
+    for fname in os.listdir("./"):
+        if fname.endswith(".xls"):
+            return fname
+    return None
+
+
+
+# Expand the window full screen
 S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
 driver.set_window_size(S('Width'), S('Height'))  # May need manual adjustment
 
+
+# take screenshot of the data
 data = driver.find_element(By.XPATH, '//*[@class = "tab-content"]').find_elements(By.XPATH,
                                                                                   './div[@class = "tab-content-hor"]')
 
 i = 1
-for element in data:
-    element.screenshot(f'web-element-{i}.png')
-    i += 1
+time.sleep(1)
+for i in range(1,6):
+    # WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f'//*[@class = "bi-board-tab"]/jhi-tab[position() = 1]//div[@class = "tab-content"]/div[@class = "tab-content-hor" and position() ={i}]'))).screenshot(f'web-element-{i}.png')
+    driver.find_element(By.XPATH, f'//*[@class = "bi-board-tab"]/jhi-tab[position() = 1]//div[@class = "tab-content"]/div[position() ={i}]').screenshot(f'web-element-{i}.png')
+    # element.screenshot(f'web-element-{i}.png')
 
-driver.find_element(By.TAG_NAME, 'body').screenshot('web_screenshot.png')
+# driver.find_element(By.TAG_NAME, 'body').screenshot('web_screenshot.png')
+
+
+# direct to email menu
+tab = driver.find_element(By.CLASS_NAME, "tabWarp")
+email_tab = tab.find_element(By.XPATH, './a[text() = "Emails"]')
+email_tab.click()
+
+# Select dates
+select_date(0, "Email", ["2022", "09", "01"])
+select_date(1, "Email", ["2022", "09", "30"])
+
+driver.find_elements(By.XPATH, "//div[@class= 'bi-filter-cont']//button")[6].click()
+
+WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@class = "bi-board-tab"]/jhi-tab[position() = 7]//div[@class = "tab-content-hor"]'))).screenshot("email.png")
+# data = driver.find_element(By.XPATH, '//*[@class = "bi-board-tab"]/jhi-tab[position() = 7]//div[@class = "tab-content-hor"]')
+# data.screenshot("email.png")
+
+
+
+
 
 # highlight(Astro, 3, "red", 2)
 # Astro.click()
-time.sleep(5)
 driver.quit()
 # //*[@id="owl-dt-picker-0"]/div[2]/owl-date-time-calendar/div[2]/owl-date-time-year-view/table/tbody/tr[3]/td[2]/span
